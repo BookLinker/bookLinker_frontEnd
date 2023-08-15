1f27c26836b09837537f6ec0f0b283a77598e57e
Johnsnim <qw506799@naver.com>
2023-08-16 03:51:36 +0900

    Refactor: views 페이지 컴포넌트 분리

--- pages/build.js
+++ pages/build.js
@@ -9,11 +9,27 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 
 import { useRouter } from "next/router";
+import TopNavigationBar from "@/components/common/topNavigationBar";
 
 function Build() {
   const router = useRouter();
 
-  return <Box>Build</Box>;
+  return (
+    <Box>
+      <TopNavigationBar />
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          height: "100vh",
+          pt: 8,
+          flexDirection: "row",
+          backgroundColor: "white",
+          flexWrap: "wrap",
+        }}
+      ></Box>
+    </Box>
+  );
 }
 
 export default Build;
